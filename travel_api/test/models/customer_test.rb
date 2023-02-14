82f546d709d03bb24b1197ce66d560f4d2c73020
daniel <daniel.uremix@gmail.com>
2023-02-14 00:10:19 -0400

    Fixtures clean

--- travel_api/test/models/customer_test.rb
+++ travel_api/test/models/customer_test.rb
@@ -9,12 +9,12 @@ class CustomerTest < ActiveSupport::TestCase
   # end
   test 'Create customer' do
     customer = Customer.new
-    # customer.person = Person.new
-    # customer.person.name = Faker::Name.name
-    # customer.person.last_name = Faker::Name.last_name
-    # customer.person.identity_document = Faker::Number.number(digits: 7)
-    # customer.person.status = Faker::Boolean.boolean
-    # customer.person.birthday = Faker::Date.birthday(min_age: 18, max_age: 65)
+    customer.person = Person.new
+    customer.person.name = Faker::Name.name
+    customer.person.last_name = Faker::Name.last_name
+    customer.person.identity_document = Faker::Number.number(digits: 7)
+    customer.person.status = Faker::Boolean.boolean
+    customer.person.birthday = Faker::Date.birthday(min_age: 18, max_age: 65)
     assert customer.save
   end
 end
