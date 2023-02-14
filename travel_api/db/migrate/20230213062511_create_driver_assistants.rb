651885fbf870ec0cb61939a883f2a6255abbbfc7
daniel <daniel.uremix@gmail.com>
2023-02-14 00:25:08 -0400

    Models and configs

--- travel_api/db/migrate/20230213062511_create_driver_assistants.rb
+++ travel_api/db/migrate/20230213062511_create_driver_assistants.rb
@@ -3,7 +3,7 @@
 class CreateDriverAssistants < ActiveRecord::Migration[7.0]
   def change
     create_table :driver_assistants do |t|
-      t.references :person, null: false, foreign_key: true
+      t.references :staff, null: false, foreign_key: true
 
       t.timestamps
     end
