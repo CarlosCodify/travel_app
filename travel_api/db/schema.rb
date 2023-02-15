3d6e7cf8528262bf97365fab8c955da86735f174
carloscodify <carlos.gutierrez@codify.com.bo>
2023-02-15 11:27:31 -0400

    Add devise token auth

--- travel_api/db/schema.rb
+++ travel_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_13_220805) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_145340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,18 +45,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_13_220805) do
   end
 
   create_table "driver_assistants", force: :cascade do |t|
-    t.bigint "person_id", null: false
+    t.bigint "staff_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["person_id"], name: "index_driver_assistants_on_person_id"
+    t.index ["staff_id"], name: "index_driver_assistants_on_staff_id"
   end
 
   create_table "drivers", force: :cascade do |t|
     t.string "driver_license"
-    t.bigint "person_id", null: false
+    t.bigint "staff_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["person_id"], name: "index_drivers_on_person_id"
+    t.index ["staff_id"], name: "index_drivers_on_staff_id"
   end
 
   create_table "manufacturers", force: :cascade do |t|
@@ -84,10 +84,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_13_220805) do
   end
 
   create_table "sale_people", force: :cascade do |t|
-    t.bigint "person_id", null: false
+    t.bigint "staff_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["person_id"], name: "index_sale_people_on_person_id"
+    t.index ["staff_id"], name: "index_sale_people_on_staff_id"
   end
 
   create_table "sales", force: :cascade do |t|
@@ -149,6 +149,31 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_13_220805) do
     t.index ["route_id"], name: "index_travels_on_route_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.string "nickname"
+    t.string "image"
+    t.string "email"
+    t.json "tokens"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  end
+
   create_table "vehicle_models", force: :cascade do |t|
     t.string "name"
     t.bigint "manufacturer_id", null: false
@@ -167,9 +192,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_13_220805) do
 
   add_foreign_key "buses", "year_manufacturers"
   add_foreign_key "customers", "people"
-  add_foreign_key "driver_assistants", "people"
-  add_foreign_key "drivers", "people"
-  add_foreign_key "sale_people", "people"
+  add_foreign_key "driver_assistants", "staffs"
+  add_foreign_key "drivers", "staffs"
+  add_foreign_key "sale_people", "staffs"
   add_foreign_key "sales", "customers"
   add_foreign_key "sales", "sale_people"
   add_foreign_key "seats", "buses"
